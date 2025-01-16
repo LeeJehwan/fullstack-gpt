7179733fbc0c68819db0c425975dc817abce269f
leejehwan <wpghks7@naver.com>
2025-01-17 01:30:25 +0900

    fix

--- app.py
+++ app.py
@@ -1,13 +1,164 @@
+from abc import ABC, abstractmethod
+from enum import Enum
 from operator import itemgetter
 
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import (
+    CacheBackedEmbeddings,
+    OpenAIEmbeddings,
+    OllamaEmbeddings,
+)
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI, ChatOllama
+from langchain.chat_models.base import BaseChatModel
+from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
-from chat_message import send_message, paint_history
-from embedding import embed_file, format_docs
-from llm import LLMModels, get_llm_model
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def get_embedding_model(option):
+    if option == LLMModels.GPT_3_5_TURBO.value:
+        return OpenAIEmbeddings()
+    elif option == LLMModels.MISTRAL.value:
+        return OllamaEmbeddings(model=option)
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file, option):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = get_embedding_model(option)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+class ChatCallbackHandler(BaseCallbackHandler):
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message = ""
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+
+class LLMModels(Enum):
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
+    MISTRAL = "mistral:latest"
+
+    @classmethod
+    def all_models(cls):
+        return [model.value for model in cls]
+
+
+class BaseLLM(ABC):
+    llm: BaseChatModel
+
+    def run(self) -> BaseChatModel:
+        self.validate_model()
+        return self.llm
+
+    @abstractmethod
+    def validate_model():
+        pass
+
+
+class OpenAIModel(BaseLLM):
+    def __init__(self, key) -> None:
+        self.key = key
+
+    def validate_model(self) -> None:
+        try:
+            self.llm = ChatOpenAI(
+                model=LLMModels.GPT_3_5_TURBO.value,
+                temperature=0.1,
+                streaming=True,
+                api_key=self.key,
+                callbacks=[
+                    ChatCallbackHandler(),
+                ],
+            )
+            with st.chat_message("ai"):
+                self.llm.invoke("hello")
+        except Exception:
+            st.sidebar.error("Please check your open api key")
+
+
+class MistralModel(BaseLLM):
+    def validate_model(self) -> None:
+        try:
+            self.llm = ChatOllama(
+                model=LLMModels.MISTRAL.value,
+                temperature=0.1,
+                streaming=True,
+                callbacks=[
+                    ChatCallbackHandler(),
+                ],
+            )
+            with st.chat_message("ai"):
+                self.llm.invoke("hello")
+        except Exception:
+            st.sidebar.error("Please check your ollama server")
+
+
+def get_llm_model(model: str, key: str = ""):
+    if st.session_state.get(model):
+        return st.session_state[model]
+
+    llm = None
+    if model == LLMModels.GPT_3_5_TURBO.value:
+        if key:
+            llm = OpenAIModel(key).run()
+    elif model == LLMModels.MISTRAL.value:
+        llm = MistralModel().run()
+
+    if llm:
+        st.session_state[model] = llm
+        return llm
 
 
 st.set_page_config(
